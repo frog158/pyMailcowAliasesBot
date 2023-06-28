a34ec25f5e2286a39dba5dee12f0aacfda6fabac
Nikolai Markov <frog158@gmail.com>
2023-06-28 16:02:05 +0300

    добавил api.py и переменные окружения

---
+++ tg_mailcow_aliases/api.py
@@ -0,0 +1,8 @@
+import os
+import requests
+
+
+DEFAULT_DOMAIN = os.getenv("DEFAULT_DOMAIN", "error")
+DEFAULT_EMAIL = os.getenv("DEFAULT_EMAIL", "error")
+SERVER_URL = os.getenv("SERVER_URL", "error")
+X_API_KEY = os.getenv("X_API_KEY", "error")
