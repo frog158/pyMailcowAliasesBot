bbe30e205208d2a9d6c2017e0f83e6ae24ffb23e
Nikolai Markov <frog158@gmail.com>
2023-06-28 19:12:33 +0300

    add logging

--- tg_mailcow_aliases/bot.py
+++ tg_mailcow_aliases/bot.py
@@ -1,5 +1,10 @@
 import os
 import telebot
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN", "error")
