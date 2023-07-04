5282962fd88a8b4f0ce59785564b1071573283b5
Nikolai Markov <frog158@gmail.com>
2023-07-04 12:26:15 +0300

    add start and help command handler in bot

--- tg_mailcow_aliases/bot.py
+++ tg_mailcow_aliases/bot.py
@@ -2,6 +2,7 @@ import os
 import telebot
 import logging
 from tg_mailcow_aliases.api import add_alias
+from tg_mailcow_aliases.sqlite import is_user_exist, get_user
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -14,6 +15,17 @@ USER_ID = os.getenv("USER_ID", "error")
 logging.info(f"USER_ID = {USER_ID}")
 
 bot = telebot.TeleBot(TG_BOT_TOKEN)
+
+
+@bot.message_handler(commands=["start", "help"])
+def start(message):
+    user_id = message.from_user.id
+    if not is_user_exist(user_id):
+        bot.reply_to(message, "403 Forbidden")
+        return
+    (_, name, domain, goto) = get_user(user_id)
+    logging.info(f"{name} {domain} {goto}")
+    bot.reply_to(message, f"Hi {name}, aliases for {domain} is goto {goto}")
 
 
 @bot.message_handler(func=lambda message: True)
