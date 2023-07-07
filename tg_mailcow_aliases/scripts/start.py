0de7238fc237041e7f32d37b8dd64e063d043fbb
Nikolai Markov <frog158@gmail.com>
2023-07-07 13:46:05 +0300

    add logging and create database at start

--- tg_mailcow_aliases/scripts/start.py
+++ tg_mailcow_aliases/scripts/start.py
@@ -1,8 +1,17 @@
 from tg_mailcow_aliases.bot import start_bot
+from tg_mailcow_aliases.sqlite import create_tabel
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
 
 
 def main():
-    print("Start bot")
+    logging.info("Create database")
+    create_tabel()
+    logging.info("Start bot")
     start_bot()
 
 
